495adf78a250ea482d876142c748fc7a6fb7407e
Jovi De Croock <jovi.decroock@formidable.com>
2019-08-30 10:37:24 +0200

    (chore) - reset prisma params

--- server/src/generated/prisma-client/index.js
+++ server/src/generated/prisma-client/index.js
@@ -20,7 +20,7 @@ var models = [
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `https://eu1.prisma.sh/jovi-de-croock-c8d40d/server/dev`,
+  endpoint: ``,
   secret: `mysecret123`
 });
 exports.prisma = new exports.Prisma();
